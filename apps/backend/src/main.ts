da8393d9dc658327c2f5534717b81ada18cea944
DenysDziubenko <dzubenko.denis@gmail.com>
2019-07-26 15:28:30 +0300

    Adds logger to backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- **/
-
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.use(morgan('dev'));
+  app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
@@ -18,4 +16,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-    

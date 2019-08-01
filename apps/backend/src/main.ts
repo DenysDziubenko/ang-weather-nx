c752559b6e1652ed317cea476d8d40063e14101a
DenysDziubenko <dzubenko.denis@gmail.com>
2019-08-01 13:20:11 +0300

    prepared for deployment2

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
   app.enableCors();
-  const port = process.env.port || 3333;
+  const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/api');
   });

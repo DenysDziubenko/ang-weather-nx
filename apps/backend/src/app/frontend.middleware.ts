72bc875ddf4016dc2614c148fa3d02700118ca56
DenysDziubenko <dzubenko.denis@gmail.com>
2019-08-01 14:23:31 +0300

    Fixes resolvePath in FrontendMiddleware

--- apps/backend/src/app/frontend.middleware.ts
+++ apps/backend/src/app/frontend.middleware.ts
@@ -1,14 +1,10 @@
 import * as path from 'path';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { ConfigData } from '@ang-weather-nx/shared-data';
-import { environment } from '../environments/environment';
 
 const allowedExt = ['.js', '.ico', '.css', '.png', '.jpg', '.woff2', '.woff', '.ttf', '.svg',];
 
-const resolvePath = (file: string) => {
-  const filePath = environment.production ? `../frontend/${file}` : `dist/apps/frontend/${file}`;
-  return path.resolve(filePath);
-};
+const resolvePath = (file: string) => path.resolve(`dist/apps/frontend/${file}`);
 
 @Injectable()
 export class FrontendMiddleware implements NestMiddleware {
